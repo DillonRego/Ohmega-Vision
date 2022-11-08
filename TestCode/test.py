47dbae53acaaf4912a4a98f42b40726f447016d8
herbie-jetson <herbie.jetson@gmail.com>
2022-11-08 09:08:28 -0800

    update

--- TestCode/test.py
+++ TestCode/test.py
@@ -8,8 +8,27 @@
 import pyrealsense2 as rs
 import numpy as np
 import cv2
+import time
 
-# Configure depth and color streams
+def get_output_layers(net):
+
+    layer_names = net.getLayerNames()
+    try:
+        output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
+    except:
+        output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+    return output_layers
+
+
+def draw_prediction(img, class_id, confidence, x, y, x_plus_w, y_plus_h):
+
+    label = str(classes[class_id])
+
+    color = COLORS[class_id]
+
+    cv2.rectangle(img, (x,y), (x_plus_w,y_plus_h), color, 2)
+
 pipeline = rs.pipeline()
 config = rs.config()
 
@@ -20,13 +39,6 @@ device = pipeline_profile.get_device()
 device_product_line = str(device.get_info(rs.camera_info.product_line))
 print(device_product_line)
 found_rgb = False
-'''for s in device.sensors:
-    if s.get_info(rs.camera_info.name) == 'RGB Camera':
-        found_rgb = True
-        break
-if not found_rgb:
-    print("The demo requires Depth camera with Color sensor")
-    exit(0)'''
 
 config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
 
@@ -37,7 +49,15 @@ else:
 
 # Start streaming
 pipeline.start(config)
+count = 0
 
+classes = None
+
+with open('yolov3.txt', 'r') as f:
+    classes = [line.strip() for line in f.readlines()]
+
+COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
+net = cv2.dnn.readNet('yolov3.weights', 'yolov3.config')
 try:
     while True:
 
@@ -61,18 +81,89 @@ try:
         # If depth and color resolutions are different, resize color image to match depth image for display
         if depth_colormap_dim != color_colormap_dim:
             resized_color_image = cv2.resize(color_image, dsize=(depth_colormap_dim[1], depth_colormap_dim[0]), interpolation=cv2.INTER_AREA)
-            images = np.hstack((resized_color_image, depth_colormap))
         else:
-            images = np.hstack((color_image, depth_colormap))
+            resized_color_image = color_image
+        starttime = time.time()
+        image = resized_color_image
+        Width = image.shape[1]
+        Height = image.shape[0]
+        scale = 0.00392
+
+        blob = cv2.dnn.blobFromImage(image, scale, (416,416), (0,0,0), True, crop=False)
+
+        net.setInput(blob)
+
+        outs = net.forward(get_output_layers(net))
+        print(time.time() - starttime)
+        class_ids = []
+        confidences = []
+        boxes = []
+        conf_threshold = 0.5
+        nms_threshold = 0.4
+
+        starttime = time.time()
+        for out in outs:
+            for detection in out:
+                scores = detection[5:]
+                class_id = np.argmax(scores)
+                confidence = scores[class_id]
+                if confidence > 0:
+                    center_x = int(detection[0] * Width)
+                    center_y = int(detection[1] * Height)
+                    w = int(detection[2] * Width)
+                    h = int(detection[3] * Height)
+                    x = center_x - w / 2
+                    y = center_y - h / 2
+                    class_ids.append(class_id)
+                    confidences.append(float(confidence))
+                    boxes.append([x, y, w, h])
 
 
+        indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
+        print(time.time() - starttime)
+        starttime = time.time()
+        for i in indices:
+            try:
+                box = boxes[i]
+            except:
+                i = i[0]
+                box = boxes[i]
+    
+            x = box[0]
+            y = box[1]
+            w = box[2]
+            h = box[3]
+            draw_prediction(image, class_ids[i], confidences[i], round(x), round(y), round(x+w), round(y+h))
+        print(time.time() - starttime)
+        images = np.hstack((image, depth_colormap))
         # Show images
         cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
         cv2.imshow('RealSense', images)
-        ###cv2.imwrite("." + "\\frame%d.jpg" % count, image)
-        cv2.waitKey(1)
-
+       # cv2.imwrite("test" + "%d.jpg" % count, color_image)
+        cv2.waitKey(30)
+        count += 1
 finally:
 
     # Stop streaming
     pipeline.stop()
+
+'''
+def get_output_layers(net):
+    
+    layer_names = net.getLayerNames()
+    try:
+        output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
+    except:
+        output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+    return output_layers
+
+
+def draw_prediction(img, class_id, confidence, x, y, x_plus_w, y_plus_h):
+
+    label = str(classes[class_id])
+
+    color = COLORS[class_id]
+
+    cv2.rectangle(img, (x,y), (x_plus_w,y_plus_h), color, 2)
+'''
