c6362b332fb179c6b716cdbaa58f4bd41b4b5c8c
laestevez <luisangel.e10@gmail.com>
2023-03-14 10:00:54 -0700

    Organizing files

--- Jetson2-Code/run_monitor.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/herbie/jetson/cpp-lidar
-source .env/bin/activate
-python monitor.py
--- cpp-lidar/graphhelper.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-from google.protobuf import text_format
-
-with open('ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt') as f:
-    txt = f.read()
-gdef = text_format.Parse(txt, tf.compat.v1.GraphDef())
-tf.io.write_graph(gdef, '.', 'model.pb', as_text=False)
